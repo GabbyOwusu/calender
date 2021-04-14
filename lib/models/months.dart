9ba9a328cd5aeebadabdc67d45873a67bc327ed6
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-04-14 14:11:35 +0100

    Update

---
+++ lib/models/months.dart
@@ -0,0 +1,13 @@
+class Months {
+  int currentMonth;
+  String monthName;
+  List<DateTime> days;
+  int numberOfDays;
+
+  Months({
+    this.monthName,
+    this.days,
+    this.numberOfDays,
+    this.currentMonth,
+  });
+}
