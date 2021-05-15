0cc3287d5a721d33954e00e86f1b4294f299bf05
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-05-15 17:48:07 +0100

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:calender/provider/calender_provider.dart';
 import 'package:calender/screens/calender_home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,12 +9,19 @@ void main() {
 
 class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Calender',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CalenderProvider>(
+          create: (contex) => CalenderProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Calender',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(),
       ),
-      home: MyHomePage(),
     );
   }
 }
