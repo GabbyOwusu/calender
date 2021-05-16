bc81e24a9e2194749c5bf7811716353da265a685
GabbyOwusu <59026711+GabbyOwusu@users.noreply.github.com>
2021-05-16 20:12:15 +0100

    Update

--- lib/provider/calender_provider.dart
+++ lib/provider/calender_provider.dart
@@ -3,34 +3,48 @@ import 'package:calender/provider/base_provider.dart';
 
 class CalenderProvider extends BaseProvider {
   DateTime dateTime = DateTime(2021);
-  List<int> months;
+  Months months = Months();
 
-  int days(DateTime date) {
-    DateTime currenMonth = DateTime(date.year, date.month, date.day);
-    DateTime nextMonth = DateTime(
-      currenMonth.year,
-      currenMonth.month + 1,
-      currenMonth.day,
+  List<int> listOfDates(int month) {
+    var now = DateTime(2021, month);
+    var totalDays = daysInMonth(now);
+    var listOfDates = new List<int>.generate(totalDays, (i) {
+      print('Day ${i + 1} ${i + 1}/1/2021');
+      return i + 1;
+    });
+    months.dates = listOfDates;
+    notifyListeners();
+    return listOfDates;
+  }
+
+  int daysInMonth(DateTime date) {
+    var firstDayThisMonth = new DateTime(date.year, date.month, date.day);
+    var firstDayNextMonth = new DateTime(
+      firstDayThisMonth.year,
+      firstDayThisMonth.month + 1,
+      firstDayThisMonth.day,
     );
-    var daysInCurrentMonth = nextMonth.difference(currenMonth).inDays;
-    var weeks = daysInCurrentMonth ~/ 7;
-    var daysLeft = daysInCurrentMonth - (weeks * 7);
-    print(daysLeft);
-    print(nextMonth.difference(currenMonth));
-    return daysInCurrentMonth;
+    var numberOfDays = firstDayNextMonth.difference(firstDayThisMonth).inDays;
+    var weeks = (numberOfDays ~/ 7);
+    var daysAfterWeeks = numberOfDays - (weeks * 7);
+    months.days = numberOfDays;
+    months.numberofWeeks = weeks;
+    months.daysLeft = daysAfterWeeks;
+    print(
+      '$firstDayThisMonth has $numberOfDays full days \n $weeks weeks and $daysAfterWeeks days',
+    );
+    notifyListeners();
+    return firstDayNextMonth.difference(firstDayThisMonth).inDays;
   }
 
-  List createCalender(Months month) {
-    DateTime now = DateTime(2021, month.currentMonth);
-    int alldays = days(now);
-    List<int> allDates = List.generate(alldays, (index) {
-      print('${index + 1} / ${month.currentMonth}/ 2021');
-      return index + 1;
-    }).toList();
-    return allDates;
-  }
+  // List createCalender(Months month) {
+  //   DateTime now = DateTime(2021, month.currentMonth);
+  //   int alldays = days(now);
+  //   List<int> allDates = List.generate(alldays, (index) {
+  //     print('${index + 1} / ${month.currentMonth}/ 2021');
+  //     return index + 1;
+  //   }).toList();
+  //   return allDates;
+  // }
 
-  void getWeekNumber() {
-    final getDays = days(DateTime(2021, 1));
-  }
 }
